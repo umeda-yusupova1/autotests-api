31e24c922b99553c760b1bb860748d75b6f2bf5a
umeda <fredypoul89@gmail.com>
2025-05-08 15:00:43 +0300

    Валидация JSON schema. Практикуемся в работе с JSON schema

--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -17,7 +17,10 @@ class ExercisesClient(APIClient):
         :param query: Словарь с courseId.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get("/api/v1/exercises", params=query)
+        return self.get(
+            url="/api/v1/exercises",
+            params=query.model_dump(by_alias=True)
+        )
 
     def get_exercise_api(self, exercise_id: str) -> Response:
         """
